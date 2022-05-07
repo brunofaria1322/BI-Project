91608c595c861d513de4863a2146da6fc646a092
Dylan Perdigão <dylanperdigao@icloud.com>
2022-05-07 16:03:42 +0100

    remove useless lines

--- insert_data_on_db.py
+++ insert_data_on_db.py
@@ -4,7 +4,7 @@ import pandas as pd
 def connect_to_db():
 	connection = psycopg2.connect(
 		user="bi2022",
-		password="in2022$$",
+		password="", # nao correr mais este script.... demora tanto
 		host="bi2022.postgres.database.azure.com",
 		port="5432",
 		database="postgres"
@@ -12,59 +12,6 @@ def connect_to_db():
 	return connection
 
 def main():
-	df = pd.read_csv('Dataset/satisfaction.csv', sep=';', header=0)
-	print(df.head())
-	person_df = df[['id','Age','Gender', 'Customer Type']]
-	print(person_df.head())
-
-	ticket_df = df[['id', 'Class', 'Type of Travel']]
-	print(ticket_df.head())
-
-	cursor = connect_to_db()
-
-	cursor.execute("SELECT * FROM ticket;")
-	ticket_query = cursor.fetchall()
-	#print(ticket_query)
-
-	cursor.execute("SELECT * FROM person;")
-	person_query = cursor.fetchall()
-	#print(person_query)
-
-	for i in range(len(df['id'])):
-		cursor.execute("""
-		select  person.id, 
-		--flight_id, 
-		--satisfaction_id, 
-		ticket.id
-		--overall_satisfaction
-		from    person,
-		flight,
-		satisfaction,
-		ticket
-		where   person.age=%s and
-		person.gender=%s and
-		person.loyalty=%s and
-		flight_class=%s and
-		type_travel=%s
-		""", (str(df['Age'][i]),str(df['Gender'][i]), str(df['Customer Type'][i]), str(df['Class'][i]), str(df['Type of Travel'][i])))
-		res = cursor.fetchall()
-		print(res)
-		cursor.close()
-	"""
-	cursor.execute(""
-	INSERT INTO participant (person_username, person_email, person_password)
-	VALUES (%s,%s,%s);
-	"", (username, email, password)
-	)
-
-	#res = cursor.fetchone()[0]
-	#cursor.close()
-	#self.connection.commit()
-
-
-
-	"""
-if __name__ == "__main__":
 	# dataset
 	df = pd.read_csv('Dataset/satisfaction.csv', sep=';', header=0)
 	print(df.head())
@@ -131,49 +78,6 @@ if __name__ == "__main__":
 			# fact table
 			overall_satisfaction = df['satisfaction_v2'][i]
 			# insert into db
-			"""
-			try:
-				cursor.execute(""
-					INSERT INTO person (id, age, gender, loyalty) 
-					VALUES (%s, %s, %s, %s)
-					ON CONFLICT DO NOTHING;
-				"", (str(person_id), str(age), str(gender), str(loyalty)))
-			except:
-				pass
-			try:
-				cursor.execute(""
-					INSERT INTO satisfaction (id,seat_comfort,time_convenience,food,gate_location,wifi_service,entertainment,booking,online_support,onboard_service,leg_room_service,baggage_handling,online_boarding,checkin,cleanliness) 
-					VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-					ON CONFLICT DO NOTHING;
-				"",(str(satisfaction_id),str(seat_comfort),str(time_convenience),str(food),str(gate_location),str(wifi_service),str(entertainment),str(booking),str(online_support),str(onboard_service),str(leg_room_service),str(baggage_handling),str(online_boarding),str(checkin),str(cleanliness)))
-			except:
-				pass
-			try:
-				cursor.execute(""
-					INSERT INTO flight (id,distance,departure_delay,arrival_delay) 
-					VALUES (%s, %s, %s, %s)
-					ON CONFLICT DO NOTHING;
-				"",(str(flight_id),str(distance),str(departure_delay),str(int(arrival_delay))))
-			except:
-				pass
-			try:
-				cursor.execute(""
-					INSERT INTO ticket (id,flight_class,type_travel) 
-					VALUES (%s, %s, %s)
-					ON CONFLICT DO NOTHING;
-				"",(str(ticket_id),str( flight_class),str(type_travel)))
-			except:
-				pass
-			try:
-				cursor.execute(""
-					INSERT INTO fact (overall_satisfaction,old_id,ticket_id,flight_id,satisfaction_id,person_id) 
-					VALUES (%s, %s, %s, %s, %s, %s)
-					ON CONFLICT DO NOTHING;
-				"",(str(overall_satisfaction),str(old_id),str(ticket_id),str(flight_id),str(satisfaction_id),str(person_id)))
-			except:
-				pass
-			connection.commit()
-			"""
 			cursor.execute("""
 					INSERT INTO person (id, age, gender, loyalty) 
 					VALUES (%s, %s, %s, %s)
@@ -199,5 +103,7 @@ if __name__ == "__main__":
 			print(e)
 		connection.commit()
 	cursor.close()
+if __name__ == "__main__":
+	main()
 
 	
