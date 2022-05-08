585804d7d9872684c3527dd13574f5a3b414a00f
Bruno Faria <45635455+brunofaria1322@users.noreply.github.com>
2022-05-08 17:18:46 +0100

    visualize_data

--- ML/ml.py
+++ ML/ml.py
@@ -5,9 +5,15 @@
 __author__ = "Bruno Faria & Dylan Perdigão"
 __date__ = "May 2022"
 
+
+import matplotlib.pyplot as plt
+import numpy as np
 from os.path import exists
 import pandas as pd
 import psycopg2
+import seaborn as sns
+from sklearn.feature_selection import SelectKBest, chi2
+
 
 def connect_to_db():
     """ 
@@ -105,9 +111,10 @@ def treat_data(df):
 
 
     ## Change object columns to int
-    print(df.dtypes)
+    #print(df.dtypes)
 
     # Check conflicting columns and object values
+    print('Non-int columns')
     for col in df.columns:
         if df[col].dtypes == 'object':
             print(col, df[col].unique())
@@ -120,19 +127,110 @@ def treat_data(df):
     df.loc[:,'flight_class'] = df['flight_class'].replace({'Eco':2, 'Business':1, 'Eco Plus':0})
     df.loc[:,'type_travel'] = df['type_travel'].replace({'Personal Travel':1,'Business travel':0})
     
-    print(df.dtypes)
+    #print(df.dtypes)
 
     return df
 
+def visualize_data(df, img_path):
+    """
+    Visualizes the data
+    
+    Parameters:
+        df: a dataframe with the data
+        img_path: the path to the images folder
+    """
+
+    print("===== Data Head =====")
+    print(df.head)
+    print("===== Data Description =====")
+    print(df.describe())
+
+    #print("===== Data y =====")
+
+    ## Overall satisfaction Counts
+    plt.figure(figsize=(10,5))
+    ax = sns.countplot(x="overall_satisfaction", data=df)
+    for p in ax.patches:
+        ax.annotate(f'\n{p.get_height()}', (p.get_x()+0.2, p.get_height()), ha='center', va='top', color='white', size=18)
+    
+    plt.savefig(img_path+"overall_satisfaction_counts.png")
+    #plt.show()
+
+    ## Correlation matrix
+    correlations = df.corr(method='pearson')
+    
+    # heatmap of correlations
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(correlations, vmin=-1, vmax=1)
+    fig.colorbar(cax)
+    ticks = np.arange(0,len(df.columns),1)
+    #ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    #ax.set_xticklabels(df.columns)
+    ax.set_yticklabels(df.columns)
+    plt.savefig(img_path+"correlation_matrix.png")
+    #plt.show()
+
+    ## Histograms
+    df.hist()
+    plt.savefig(img_path+"histgrams.png")
+    #plt.show()
+
+    ## Density Plots
+    df.plot(kind='density', subplots=True, layout=(5,5), sharex=False, sharey=False)
+    plt.savefig(img_path+"density.png")
+    plt.show()
+
+
+def feature_selection(df):
+    """
+    Performs feature selection
+
+    Parameters:
+        df: a dataframe with the data
+
+    Returns:
+        us:
+    """
+
+    Y=df['overall_satisfaction']
+    X=df.drop(['overall_satisfaction'], axis=1)
+
+
+    ## Univariate Statistical Tests (Chi-squared for classification)
+    test= SelectKBest(score_func=chi2, k=4)
+    fit = test.fit(X, Y)
+    #print(fit)
+
+    # sumarize scores
+    #print(fit.scores_)
+    us = fit.transform(X)
+
+    # summarize selected features
+    print(us[0:5,:])
+
+
+    rfe = pca = fi = 0
+
+    return [us, rfe, pca, fi]
 
 
 def main():
+    pd.set_option('precision', 3)
+    plt.rcParams.update({'font.size': 8})
+    IMG_PATH = "ML/img"
 
     data = read_data()
-    print(data.shape)
+    #print(data.shape)
 
     data = treat_data(data)
-    print(data.shape)
+    #print(data.shape)
+
+    visualize_data(data,IMG_PATH)
+
+
+    [us, rfe, pca, fi] = feature_selection(data)
 
 
 
