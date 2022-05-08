979258b0d9f5eb39648848899ab70cdafa0fc32a
Bruno Faria <45635455+brunofaria1322@users.noreply.github.com>
2022-05-08 02:55:06 +0100

    db connection

--- ML/ml.py
+++ ML/ml.py
@@ -5,11 +5,34 @@
 __author__ = "Bruno Faria & Dylan Perdigão"
 __date__ = "May 2022"
 
+
+import psycopg2
+
+def connect_to_db():
+	return psycopg2.connect(
+		user="bi2022",
+		password="in2022$$",
+		host="bi2022.postgres.database.azure.com",
+		port="5432",
+		database="postgres"
+	)
+
 def read_data():
-    pass
+    connection = connect_to_db()
+
+    print(connection)
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM auction")
+    print(cursor.fetchall())
+    cursor.close()
+
+    connection.close()
+
 
 def main():
-    pass
+    data = read_data()
+    
 
 if __name__ == "__main__":
     main()
