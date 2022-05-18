64feca93f558528303f1aea2db221de2312802aa
Dylan Perdigão <dylanperdigao@icloud.com>
2022-05-18 10:00:03 +0100

    muita coisa

---
+++ Talend-Meta2/poms/jobs/process/setup_wh_1.0/src/main/templates/Job_run_template.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib -Xms2048M -Xmx2048M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/log4j-slf4j-impl-2.12.1.jar:$ROOT_PATH/../lib/log4j-api-2.12.1.jar:$ROOT_PATH/../lib/log4j-core-2.12.1.jar:$ROOT_PATH/../lib/postgresql-42.2.9.jar:$ROOT_PATH/../lib/crypto-utils.jar:$ROOT_PATH/../lib/slf4j-api-1.7.25.jar:$ROOT_PATH/../lib/dom4j-2.1.1.jar:$ROOT_PATH/setup_wh_1_0.jar: bi.setup_wh_1_0.Setup_WH  "$@"
--- Talend/poms/jobs/process/Tasks/dim_table_creation_0.1/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms4096M' '-Xmx4096M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/OneWireAPI.jar;../lib/postgresql-42.2.9.jar;../lib/talendcsv.jar;../lib/crypto-utils.jar;../lib/talend_file_enhanced_20070724.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;dim_table_creation_0_1.jar;' bi.dim_table_creation_0_1.Dim_Table_Creation  $args
